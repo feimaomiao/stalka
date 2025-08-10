5f9401ec9d38c0e271beefdf5fb61fb25e7757f4
Cheuk Pui Lam <lcpmatthew@gmail.com>
2025-08-10 14:22:22 -0700

    tests: added first database test

--- go.mod
+++ go.mod
@@ -12,10 +12,21 @@ require (
 
 require (
 	github.com/h2non/parth v0.0.0-20190131123155-b4df798d6542 // indirect
+	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
+	github.com/jackc/pgconn v1.14.3 // indirect
+	github.com/jackc/pgio v1.0.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgproto3/v2 v2.3.3 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/pgtype v1.14.4 // indirect
+	github.com/jackc/pgx/v4 v4.18.3 // indirect
+	github.com/jackc/puddle v1.3.0 // indirect
+	github.com/jackc/puddle/v2 v2.2.2 // indirect
+	github.com/pashagolub/pgxmock v1.8.0 // indirect
+	github.com/pashagolub/pgxmock/v4 v4.8.0 // indirect
 	github.com/stretchr/testify v1.9.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
-	golang.org/x/crypto v0.37.0 // indirect
-	golang.org/x/text v0.24.0 // indirect
+	golang.org/x/crypto v0.41.0 // indirect
+	golang.org/x/sync v0.16.0 // indirect
+	golang.org/x/text v0.28.0 // indirect
 )
--- pandatypes/datatypes_test.go
+++ pandatypes/datatypes_test.go
@@ -3,10 +3,16 @@ package pandatypes
 import (
 	"encoding/json"
 	"fmt"
+	"math"
 	"os"
 	"testing"
 
+	"github.com/feimaomiao/stalka/dbtypes"
+	"github.com/jackc/pgx/v5/pgtype"
 	"github.com/nbio/st"
+
+	// "github.com/pashagolub/pgxmock"
+	"github.com/pashagolub/pgxmock/v4"
 )
 
 func TestSafeIntToInt32(t *testing.T) {
@@ -43,4 +49,161 @@ func TestToRow(t *testing.T) {
 	st.Assert(t, gameR.ID, game.ID)
 	st.Assert(t, gameR.Name, game.Name)
 	st.Assert(t, gameR.Slug, game.Slug)
+
+	// Test leagues
+	data, err = os.ReadFile("../static/fetch_data/leagues.json")
+	st.Assert(t, err, nil)
+
+	var league LeagueLike
+	err = json.Unmarshal(data, &league)
+	st.Assert(t, err, nil)
+
+	st.Assert(t, league.ID, 289)
+	st.Assert(t, league.Name, "NA LCS")
+	st.Assert(t, league.Slug, "league-of-legends-na-lcs")
+
+	// assert that league can be converted to LeagueRow
+	leagueR := league.ToRow().(LeagueRow)
+	st.Assert(t, recover(), nil)
+
+	// Assert the row data
+	st.Assert(t, leagueR.ID, league.ID)
+	st.Assert(t, leagueR.Name, league.Name)
+	st.Assert(t, leagueR.Slug, league.Slug)
+	st.Assert(t, leagueR.GameID, league.Videogame.ID)
+	st.Assert(t, leagueR.ImageLink, league.ImageURL)
+
+	// Test series
+	data, err = os.ReadFile("../static/fetch_data/series.json")
+	st.Assert(t, err, nil)
+
+	var series SeriesLike
+	err = json.Unmarshal(data, &series)
+	st.Assert(t, err, nil)
+
+	st.Assert(t, series.ID, 346)
+	st.Assert(t, series.Name, "")
+	st.Assert(t, series.Slug, "league-of-legends-international-wildcard-msi-qualifier-2016")
+
+	// assert that series can be converted to SeriesRow
+	seriesR := series.ToRow().(SeriesRow)
+	st.Assert(t, recover(), nil)
+
+	// Assert the row data
+	st.Assert(t, seriesR.ID, series.ID)
+	st.Assert(t, seriesR.Name, series.Name)
+	st.Assert(t, seriesR.Slug, series.Slug)
+	st.Assert(t, seriesR.GameID, series.Videogame.ID)
+	st.Assert(t, seriesR.LeagueID, series.League.ID)
+
+	// Test tournaments
+	data, err = os.ReadFile("../static/fetch_data/tournaments.json")
+	st.Assert(t, err, nil)
+
+	var tournament TournamentLike
+	err = json.Unmarshal(data, &tournament)
+	st.Assert(t, err, nil)
+
+	st.Assert(t, tournament.ID, 17283)
+	st.Assert(t, tournament.Name, "Group Stage")
+	st.Assert(t, tournament.Slug, "the-international-2025-group-stage")
+
+	// assert that tournament can be converted to TournamentRow
+	tournamentR := tournament.ToRow().(TournamentRow)
+	st.Assert(t, recover(), nil)
+
+	// Assert the row data
+	st.Assert(t, tournamentR.ID, tournament.ID)
+	st.Assert(t, tournamentR.Name, tournament.Name)
+	st.Assert(t, tournamentR.Slug, tournament.Slug)
+	st.Assert(t, tournamentR.GameID, tournament.Videogame.ID)
+	st.Assert(t, tournamentR.SerieID, tournament.Serie.ID)
+	st.Assert(t, tournamentR.LeagueID, tournament.League.ID)
+
+	// Test teams
+	data, err = os.ReadFile("../static/fetch_data/teams.json")
+	st.Assert(t, err, nil)
+
+	var team TeamLike
+	err = json.Unmarshal(data, &team)
+	st.Assert(t, err, nil)
+
+	st.Assert(t, team.ID, 127652)
+	st.Assert(t, team.Name, "Ares Gaming")
+	st.Assert(t, team.Slug, "ares-gaming")
+
+	// assert that team can be converted to TeamRow
+	teamR := team.ToRow().(TeamRow)
+	st.Assert(t, recover(), nil)
+
+	// Assert the row data
+	st.Assert(t, teamR.ID, team.ID)
+	st.Assert(t, teamR.Name, team.Name)
+	st.Assert(t, teamR.Slug, team.Slug)
+	st.Assert(t, teamR.GameID, team.CurrentVideogame.ID)
+	st.Assert(t, teamR.Acronym, team.Acronym)
+	st.Assert(t, teamR.ImageLink, team.ImageURL)
+
+	// Test matches
+	data, err = os.ReadFile("../static/fetch_data/matches.json")
+	st.Assert(t, err, nil)
+
+	var match MatchLike
+	err = json.Unmarshal(data, &match)
+	st.Assert(t, err, nil)
+
+	st.Assert(t, match.ID, 21655)
+	st.Assert(t, match.Name, "Qf 2")
+	st.Assert(t, match.Slug, "team-lolpro-vs-area-of-effect-esports-2014-06-01")
+
+	// assert that match can be converted to MatchRow
+	matchR := match.ToRow().(MatchRow)
+	st.Assert(t, recover(), nil)
+
+	// Assert the row data
+	st.Assert(t, matchR.ID, match.ID)
+	st.Assert(t, matchR.Name, match.Name)
+	st.Assert(t, matchR.Slug, match.Slug)
+	st.Assert(t, matchR.GameID, match.Videogame.ID)
+	st.Assert(t, matchR.LeagueID, match.League.ID)
+	st.Assert(t, matchR.SerieID, match.Serie.ID)
+	st.Assert(t, matchR.TournamentID, match.Tournament.ID)
+
+}
+
+func TestWriteToDB(t *testing.T) {
+	// open the file static/fetch_data/videogames.json with os.readfile
+	data, err := os.ReadFile("../static/fetch_data/videogames.json")
+	st.Assert(t, err, nil)
+
+	var game GameLike
+	err = json.Unmarshal(data, &game)
+	st.Assert(t, err, nil)
+	row := game.ToRow()
+	// create mock db with pgxmock
+	mockDB, err := pgxmock.NewPool()
+	st.Assert(t, err, nil)
+	defer mockDB.Close()
+
+	mockDB.ExpectExec("INSERT INTO games").
+		WithArgs(
+			int32(game.ID),
+			game.Name,
+			pgtype.Text{
+				String: game.Slug,
+				Valid:  true,
+			}).
+		WillReturnResult(
+			pgxmock.NewResult("INSERT", 1),
+		)
+
+	mockQuery := dbtypes.New(mockDB)
+	err = row.WriteToDB(t.Context(), mockQuery)
+	st.Expect(t, err, nil)
+	st.Expect(t, mockDB.ExpectationsWereMet(), nil)
+	// this following test should fail because the game ID is out of range
+	game.ID = math.MaxInt32 + 1
+	err = game.ToRow().WriteToDB(t.Context(), mockQuery)
+	st.Reject(t, err, nil)
+
 }
